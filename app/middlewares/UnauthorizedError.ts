fd3023b03dad39a5f8807c48bfe1e12175593b39
CNBlackJ <yes.heng@icloud.com>
2017-08-05 10:37:43 +0800

    fix token validate

--- app/middlewares/UnauthorizedError.ts
+++ app/middlewares/UnauthorizedError.ts
@@ -1,5 +1,5 @@
 export function UnauthorizedError(err: any, req: any, res: any, next: any) {
   if (err.name === 'UnauthorizedError') {
-    res.apiError({ error_code: 401, error_msg: 'Invalid token!' })
+    res.send('Invalid token!')
   }
 }
